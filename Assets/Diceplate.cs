4c997894bb9fe1c57d845b823758cf0b18f48c84
frank870622 <frank870622@gmail.com>
2018-05-31 21:30:54 +0800

    dice

---
+++ Assets/Diceplate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diceplate : MonoBehaviour {
+    public Camera camera;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3(camera.transform.position.x, camera.transform.position.y - 20, camera.transform.position.z);
+    }
+}
