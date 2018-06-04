13fb8bbcc98bce4ab5a2c2fbb6dacca0308c7544
frank870622 <frank870622@gmail.com>
2018-06-04 22:46:15 +0800

    map image

---
+++ Assets/MainCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x > 178.6)
+        {
+            transform.position = new Vector3((float)178.6, transform.position.y, transform.position.z);
+        }
+        if(transform.position.z > 137.9)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, (float)137.9);
+        }
+        if(transform.position.x < -178.6)
+        {
+            transform.position = new Vector3((float)-178.6, transform.position.y, transform.position.z);
+        }
+        if(transform.position.z < -137.9)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, (float)-137.9);
+        }
+	}
+}
