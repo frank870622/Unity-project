bfc1cbd286acaeb940c27ac51e2d425e3b9f4726
frank870622 <frank870622@gmail.com>
2018-06-07 21:16:23 +0800

    beta version is finish!!

---
+++ Assets/BuyButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class BuyButton : MonoBehaviour {
+    public GameSystem gameSystem;
+	public void buyButtonClick()
+    {
+        if(transform.GetComponent<Button>().interactable == true) gameSystem.buyButtonClick();
+        transform.GetComponent<Button>().interactable = false;
+    }
+}
---
+++ Assets/ScoreLabel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ScoreLabel : MonoBehaviour {
+
+	public void setLabel(Player player)
+    {
+        transform.GetComponent<Text>().text = "金錢: " + player.getMoney() + "\n分數:" + player.getscore();
+    }
+}
